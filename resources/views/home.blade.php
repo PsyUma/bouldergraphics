28c5495d6b291052f6e34d7cfaa758f61764d508
Michael Boyd <michaelboyd.08@gmail.com>
2017-09-19 07:47:56 -0700

    Front end updates for splash area on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,42 @@
 
 <div class="home-page-container inner-container">
 
-    <div class="splash-container text-center">
+    <div class="splash-container">
 
-        <div class="splash-text">Simplifying Your 3D Modeling Designs</div>
+        <div class="splash-image-background-container">
+
+            <div class="splash-image-container form-group-no-margin col-sm-6">
+                <div class="splash-image text-center">
+                    <img class="splash-image-icon" src="/assets/images/splash-image.png" />
+                </div>
+            </div>
+
+            <div class="splash-action-container form-group-no-margin col-sm-6">
+
+                <div class="action-container text-center">
+
+                    <div class="logo-icon-container">
+                        <img class="logo-icon" src="/assets/images/logo.png" />
+                    </div>
+
+                    <div class="app-tag-line-container text-center">
+                        <span class="splash-text">The worlds most advanced Sub Division modeling software</span>
+                    </div>
+
+                    <div class="app-download-container">
+
+                        <button type="button" class="btn btn-boulder btn-download-superd-app">
+                            <a href="http://appstore.com/superd" target="_blank">Download SuperD</a>
+                        </button>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 
     </div>
 
