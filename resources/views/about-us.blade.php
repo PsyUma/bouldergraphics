cbf732747a7bee5ba04810507fb9aab64c3ece1c
Michael Boyd <michaelboyd.08@gmail.com>
2017-09-24 19:53:15 -0700

    Add in About Us template page. More front end updates

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="about-us-page-container inner-container container">
+
+    <div class="about-us-title-container">
+        <span class="about-us-title-text">Coming Soon</span>
+    </div>
+
+</div>
+
+@stop
+
+@section('scripts')
+@include('layouts.scripts')
+@stop
