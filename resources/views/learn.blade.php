72f76faac70f7ab6b8b24567c6efafbe7b02cb0c
Michael Boyd <michaelboyd.08@gmail.com>
2017-10-07 20:26:44 -0700

    Add in new video to learning center. Front end updates to support dynamic content

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -13,7 +13,7 @@
             <div class="demo-videos-description-container">
 
                 <div class="demo-videos-title">
-                    <span class="demo-videos-title-text">Demo Videos</span>
+                    <div class="demo-videos-title-text">Demo Videos</div>
                 </div>
 
             </div>
@@ -27,6 +27,20 @@
                 <div class="demo-video">
                     <iframe class="demo-video-iframe" width="600" height="400"
                         src="https://www.youtube.com/embed/mf-dQ9k9-X0">
+                    </iframe>
+                </div>
+
+            </div>
+
+            <div class="smooth-subd-modeling-content demo-video-content">
+
+                <div class="video-title">
+                    <span class="video-title-text">Modeling on iOS</span>
+                </div>
+
+                <div class="demo-video">
+                    <iframe class="demo-video-iframe" width="600" height="400"
+                        src="https://www.youtube.com/embed/VcckvPj2z9E">
                     </iframe>
                 </div>
 
