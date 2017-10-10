634d26011d1f59b6e6ab0cb61acb9967469bae5e
Michael Boyd <michaelboyd.08@gmail.com>
2017-10-10 10:10:49 -0700

    Add in two more videos to learning center. CSS update

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -21,12 +21,26 @@
             <div class="sake-cup-content demo-video-content">
 
                 <div class="video-title">
-                    <span class="video-title-text">How to Model a Sake cup</span>
+                    <span class="video-title-text">How to Model a Sake Cup</span>
                 </div>
 
                 <div class="demo-video">
                     <iframe class="demo-video-iframe" width="600" height="400"
                         src="https://www.youtube.com/embed/mf-dQ9k9-X0">
+                    </iframe>
+                </div>
+
+            </div>
+
+            <div class="sake-bottle-content demo-video-content">
+
+                <div class="video-title">
+                    <span class="video-title-text">How to Model a Sake Bottle</span>
+                </div>
+
+                <div class="demo-video">
+                    <iframe class="demo-video-iframe" width="600" height="400"
+                        src="https://www.youtube.com/embed/EPf5clqB3d0">
                     </iframe>
                 </div>
 
@@ -41,6 +55,20 @@
                 <div class="demo-video">
                     <iframe class="demo-video-iframe" width="600" height="400"
                         src="https://www.youtube.com/embed/VcckvPj2z9E">
+                    </iframe>
+                </div>
+
+            </div>
+
+            <div class="design-session-content demo-video-content">
+
+                <div class="video-title">
+                    <span class="video-title-text">Design Session Example</span>
+                </div>
+
+                <div class="demo-video">
+                    <iframe class="demo-video-iframe" width="600" height="400"
+                        src="https://www.youtube.com/embed/IUX35b9hItg">
                     </iframe>
                 </div>
 
